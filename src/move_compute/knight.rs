9c9a8a4a8c44ba66fa8325c53dd8c1dd843924f7
Robin Moquet <robin.moquet@pm.me>
2026-04-19 11:01:54 +0200

    feat: end knight moves possibilities

--- src/move_compute/knight.rs
+++ src/move_compute/knight.rs
@@ -1,7 +1,39 @@
 use crate::types::{GameState, Piece, Position};
 
 pub fn knight_move_possibilities(game: &GameState, piece: &Piece, pos: &Position) -> Vec<Position> {
-    vec![Position::new(0, 0)]
+    let mut res = Vec::new();
+    let possibilities: [(i8, i8); 8] = [
+        (-2, -1),
+        (-1, -2),
+        (1, -2),
+        (2, -1),
+        (2, 1),
+        (1, 2),
+        (-1, 2),
+        (-2, 1),
+    ];
+
+    for (col, row) in possibilities {
+        let (col, row) = (pos.col as i8 + col, pos.row as i8 + row);
+
+        let is_in_board = 0 <= col && col < 8 && 0 <= row && row < 8;
+
+        // check if possiblities is in board and if square is empty or use by other color
+        if is_in_board
+            && (game.board.squares[row as usize][col as usize]
+                .piece
+                .is_none()
+                || game.board.squares[row as usize][col as usize]
+                    .piece
+                    .unwrap()
+                    .color
+                    != piece.color)
+        {
+            res.push(Position::new(col as u8, row as u8));
+        }
+    }
+
+    res
 }
 
 #[cfg(test)]
@@ -35,45 +67,45 @@ mod tests {
         let piece = Piece::new(PieceKind::Knight, Color::Black);
 
         assert_eq!(
-            vec![str_to_pos("a6").unwrap(), str_to_pos("c6").unwrap()],
+            vec![str_to_pos("c6").unwrap(), str_to_pos("a6").unwrap()],
             knight_move_possibilities(&game, &piece, &str_to_pos("b8").unwrap())
         );
 
         assert_eq!(
-            vec![str_to_pos("f6").unwrap(), str_to_pos("h6").unwrap()],
+            vec![str_to_pos("h6").unwrap(), str_to_pos("f6").unwrap()],
             knight_move_possibilities(&game, &piece, &str_to_pos("g8").unwrap())
         );
     }
 
     #[test]
     fn white_knight_all_moves() {
-        let mut game = initialize(Some("7k/8/3n4/8/8/3N4/8/7K w - - 0 1".to_string()));
+        let game = initialize(Some("7k/8/3n4/8/8/3N4/8/7K w - - 0 1".to_string()));
         let wpiece = Piece::new(PieceKind::Knight, Color::White);
         let bpiece = Piece::new(PieceKind::Knight, Color::Black);
 
         assert_eq!(
             vec![
                 str_to_pos("b4").unwrap(),
-                str_to_pos("b2").unwrap(),
                 str_to_pos("c5").unwrap(),
-                str_to_pos("c1").unwrap(),
                 str_to_pos("e5").unwrap(),
-                str_to_pos("e1").unwrap(),
                 str_to_pos("f4").unwrap(),
-                str_to_pos("f2").unwrap()
+                str_to_pos("f2").unwrap(),
+                str_to_pos("e1").unwrap(),
+                str_to_pos("c1").unwrap(),
+                str_to_pos("b2").unwrap(),
             ],
             knight_move_possibilities(&game, &wpiece, &str_to_pos("d3").unwrap())
         );
         assert_eq!(
             vec![
                 str_to_pos("b7").unwrap(),
-                str_to_pos("b5").unwrap(),
                 str_to_pos("c8").unwrap(),
-                str_to_pos("c4").unwrap(),
                 str_to_pos("e8").unwrap(),
-                str_to_pos("e4").unwrap(),
                 str_to_pos("f7").unwrap(),
-                str_to_pos("f5").unwrap()
+                str_to_pos("f5").unwrap(),
+                str_to_pos("e4").unwrap(),
+                str_to_pos("c4").unwrap(),
+                str_to_pos("b5").unwrap(),
             ],
             knight_move_possibilities(&game, &bpiece, &str_to_pos("d6").unwrap())
         );
@@ -88,36 +120,36 @@ mod tests {
         assert_eq!(
             vec![
                 str_to_pos("b6").unwrap(),
-                str_to_pos("b2").unwrap(),
                 str_to_pos("c5").unwrap(),
                 str_to_pos("c3").unwrap(),
+                str_to_pos("b2").unwrap(),
             ],
             knight_move_possibilities(&game, &wpiece, &str_to_pos("a4").unwrap())
         );
         assert_eq!(
             vec![
+                str_to_pos("f5").unwrap(),
                 str_to_pos("g6").unwrap(),
                 str_to_pos("g2").unwrap(),
-                str_to_pos("f5").unwrap(),
                 str_to_pos("f3").unwrap(),
             ],
             knight_move_possibilities(&game, &wpiece, &str_to_pos("h4").unwrap())
         );
         assert_eq!(
             vec![
-                str_to_pos("b3").unwrap(),
                 str_to_pos("b7").unwrap(),
+                str_to_pos("c6").unwrap(),
                 str_to_pos("c4").unwrap(),
-                str_to_pos("c6").unwrap()
+                str_to_pos("b3").unwrap(),
             ],
             knight_move_possibilities(&game, &bpiece, &str_to_pos("a5").unwrap())
         );
         assert_eq!(
             vec![
-                str_to_pos("g3").unwrap(),
+                str_to_pos("f6").unwrap(),
                 str_to_pos("g7").unwrap(),
+                str_to_pos("g3").unwrap(),
                 str_to_pos("f4").unwrap(),
-                str_to_pos("f6").unwrap()
             ],
             knight_move_possibilities(&game, &bpiece, &str_to_pos("h5").unwrap())
         );
@@ -213,7 +245,6 @@ mod tests {
                 str_to_pos("e5").unwrap(),
                 str_to_pos("c5").unwrap(),
                 str_to_pos("b6").unwrap(),
-                str_to_pos("b8").unwrap(),
             ],
             knight_move_possibilities(&game, &bpiece, &str_to_pos("d7").unwrap())
         );
